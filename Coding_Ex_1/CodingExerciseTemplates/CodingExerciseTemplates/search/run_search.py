0d42ec081c6ae1f5591c74cc9bd237b291f91b00
Bhaskar Sah <ge62new@mytum.de>
2026-01-15 23:37:06 +0100

    added .gitignore file and implemented depth_first_search algorithm

--- Coding_Ex_1/CodingExerciseTemplates/CodingExerciseTemplates/search/run_search.py
+++ Coding_Ex_1/CodingExerciseTemplates/CodingExerciseTemplates/search/run_search.py
@@ -11,32 +11,32 @@ class Agent():
     #implementHere
     def depth_first_search(self):
         # TODO initialize a stack
-        stack =
+        stack = util.Stack()
         # TODO get the start state of the environment and push it on the stack
-        start_state =
+        start_state = self.env.get_start_state()
         stack
         # TODO initialize a ClosedList called closed_list
-        closed_list =
+        closed_list = util.ClosedList()
         while stack.get_number_of_entries() > 0:
             # TODO get the first element of the stack
-            s =
+            s = stack.pop()
             print("Expand node {}".format(s))
             # TODO if the closed_list does not contain state s already
-            if
+            if closed_list.contains(s) is False:
                 # TODO add state s to the closed_list
-                closed_list.
+                closed_list.contains(s)
                 # TODO for every child_tuple in the list of successors from s
-                for
+                for child_tuple in self.env.get_successors(s):
                     # TODO extract the child, action, reward from the variable child_tuple
-                    child, action, reward
+                    child, action, reward = child_tuple
                     # TODO add the child and parent node s to the graph
-                    self.graph
+                    self.graph.add(child=child, parent=s)
                     # TODO if child node is goal node
-                    if
+                    if self.env.is_goal(child):
                         # TODO return actual solution from the graph with the actual child as input
-                        return self.graph.
+                        return self.graph.get_actual_solution(child)
                     # TODO push the child on the stack
-                    stack.
+                    stack.push(child)
                     print("Add node {} to stack".format(child))
 
     # implementHere
